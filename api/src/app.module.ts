f067b342860b48aae9b9351b0f3bbf6dbddfeef5
Jonathan W. <150624002+marte2050@users.noreply.github.com>
2025-02-19 19:39:49 -0300

    :card_file_box: Configuração para acesso ao mongodb.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssetsModule } from './assets/assets.module';
+import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(''), AssetsModule, WalletsModule],
   controllers: [AppController],
   providers: [AppService],
 })
