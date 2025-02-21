88d6553d4286ff31b5f9fd08b69c314d0fd4aea9
Jonathan W. <150624002+marte2050@users.noreply.github.com>
2025-02-20 22:07:31 -0300

    :sparkles: Feat - cria uma camada de apresentação.

--- api/src/assets/assets.service.ts
+++ api/src/assets/assets.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AssetPresenter } from './assets.presenter';
 import { CreateAssetDto } from './dto/create-asset.dto';
 import { Asset } from './entities/asset.entity';
 
@@ -8,15 +9,18 @@ import { Asset } from './entities/asset.entity';
 export class AssetsService {
   constructor(@InjectModel(Asset.name) private assetSchema: Model<Asset>) {}
 
-  create(createAssetDto: CreateAssetDto) {
-    return this.assetSchema.create(createAssetDto);
+  async create(createAssetDto: CreateAssetDto) {
+    const asset = await this.assetSchema.create(createAssetDto);
+    return new AssetPresenter(asset);
   }
 
-  findAll() {
-    return this.assetSchema.find();
+  async findAll() {
+    const assets = await this.assetSchema.find();
+    return assets.map((asset) => new AssetPresenter(asset));
   }
 
-  findOne(symbol: string) {
-    return this.assetSchema.findOne({ symbol: symbol });
+  async findOne(symbol: string) {
+    const asset = await this.assetSchema.findOne({ symbol: symbol });
+    return new AssetPresenter(asset!);
   }
 }
